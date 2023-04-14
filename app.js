cc28bc4e6da367233cc35dbd0f67999b38760e17
vosirandr <61714173+vosirandr@users.noreply.github.com>
2023-04-14 11:05:38 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+// npm install express <view engine>
+const express = require("express");
+  
+const app = express();
+  
+app.set("view engine", "pug");
+//app.set("views", "templates"); // установка пути к представлениям. По умолчанию - views
+ 
+app.use("/", function(request, response){
+      
+    response.render("index");
+}); 
+ 
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
